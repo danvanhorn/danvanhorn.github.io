7fc94feac3ee505f48eed1827eca8f871b650c32
danvanhorn <dan.vanhorn17@gmail.com>
2017-12-18 18:45:28 -0800

    update resume content

---
+++ src/site-content/education.js
@@ -0,0 +1,16 @@
+export default education = [
+    {
+      name: "Oregon State University",
+      town: "Corvallis OR",
+      duration: "September 2015 - June 2019",
+      degree: "B.S. Computer Science, Computer Systems",
+      gpa: 3.5
+    },
+    {
+      name: "Chemeketa Community College",
+      town: "Salem OR",
+      duration: "September 2013 - June 2014 ",
+      degree: "Associates Degree",
+      gpa: 3.27
+    }
+  ];
